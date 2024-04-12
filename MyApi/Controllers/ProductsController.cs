ac2e1409a07a5212aecfa1518ef490552f4011f9
shkholikov <imshakhzod@gmail.com>
2024-04-12 09:58:58 +0200

    Init commit

---
+++ MyApi/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetAllProducts()
+        {
+            return Ok(_context.Products.ToArray());
+        }
+
+        [HttpGet]
+        public ActionResult GetProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            return Ok(product);
+        }
+    }
+}
--- MyApi/Program.cs
+++ MyApi/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using HPlusSport.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ShopContext>(options =>
+{
+    options.UseInMemoryDatabase("Shop");
+});
 
 var app = builder.Build();
 
@@ -23,4 +31,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
